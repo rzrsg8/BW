4766263af2dcb87f9b06b2a35e83fa6e3caf8a76
rzrsg8 <lukasz.jurkiewicz125@gmail.com>
2024-04-29 16:39:42 +0200

    check

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/BW/', // Set the base to match your GitHub Pages path
+  base: '/', // Set the base to match your GitHub Pages path
   plugins: [react()],
 })
