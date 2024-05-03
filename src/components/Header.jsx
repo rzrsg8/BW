e8c3d692dd341a8b3ea0fa014a1772ebca98ca89
rzrsg8 <lukasz.jurkiewicz125@gmail.com>
2024-05-03 16:17:26 +0200

    ton connection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,19 +68,6 @@ const Header = () => {
           <HamburgerMenu />
         </nav>
 
-       <ul className="flex gap-5 flex-wrap">
-          {socials.map((item) => (
-            <a
-              key={item.id}
-              href={item.url}
-              target="_blank"
-              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
-            >
-              <img src={item.iconUrl} width={16} height={16} alt={item.title} />
-            </a>
-          ))}
-        </ul>
-        
         <Button className="hidden lg:flex" href="https://app.ston.fi/swap?chartVisible=false&ft=TON&tt=EQAqoUtP_bHHRoysYdxFhXhLxcvtBIICP3hXuiHeX5J8e5-3">
           Buy Token 
         </Button>
